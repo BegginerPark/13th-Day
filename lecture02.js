91a4e8b8a423c3197fada17defee01fa068a0ed6
BegginerPark <78770250+BegginerPark@users.noreply.github.com>
2021-03-04 18:03:40 +0900

    Add files via upload

---
+++ lecture02.js
@@ -0,0 +1,3 @@
+console.log("Hello develper consol");
+console.log("2");
+console.log("3");
---
+++ lecture05.js
@@ -0,0 +1,8 @@
+var a = 100, b = 3.14;
+var c = "안녕하세요", d = "a";
+var e = true, f = false;
+
+console.log(a,typeof(b));
+console.log(c,typeof(d));
+console.log(e,typeof(f));
+
